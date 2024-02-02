edbb8b8a86b504dc4dbe57f83c3027a79e8d43a1
Pavel.K144 <portex144@gmail.com>
2024-02-02 14:07:47 +0100

    AudioManager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public static AudioManager instance;
+    private List<GameObject> audioList;
+    // Start is called before the first frame update
+    void Awake() // Si no se pone "private" o "public" siempre es "private" por defecto
+    {
+
+        if (instance)
+        {
+            instance = this;
+            Destroy(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        //Instanciamos la lista
+        audioList = new List<GameObject>();
+    }
+
+    //Recreando el BackGroundMusic desde el codigo
+    public AudioSource PlayAudio(AudioClip audioClip, string gameObjectName, bool isLoop = false, float volume = 1.0f)
+    {
+        //Creamos un objecto
+        GameObject audioObject = new GameObject(gameObjectName);
+        //Hijos del audiomanager, objectos creados
+        audioObject.transform.SetParent(transform);
+        //Añadimos el componente de AudioSource y le añadimos el clip de audio
+        AudioSource srcComponent = audioObject.AddComponent<AudioSource>();
+        //Asignamos el clip
+        srcComponent.clip = audioClip;
+        // El clip esta bucle
+        srcComponent.loop = isLoop;
+        //Empieza a sonar
+        srcComponent.Play();
+        //Añadimos un objecto a la lista para hacer un seguimiento
+        audioList.Add(audioObject);
+
+
+        //devuelve el componente para dar libertad a la hora de editarlo
+        return srcComponent;
+    }
+
+    public void ClearAudios()
+    {
+        foreach(GameObject audioObjet in audioList)
+        {
+            Destroy(audioObjet);
+        }
+
+        audioList.Clear();
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,31 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public enum GameManagerVariables { TIME, POINTS };
+
     private float time;
+    private int points;
 
-    public void Awake()
+    private void Awake()
     {
-        //SINGLETON
-        if (!instance) //si instance no tiene info
+        // SINGLETON
+        if (!instance) // si instance no tiene informacion
         {
-            instance = this; //instance se asigna a este objeto
-            DontDestroyOnLoad(gameObject); // se indica que este obj no se destruya  con la carga de escenas
+            instance = this; // instance se asigna a este objeto
+            DontDestroyOnLoad(gameObject); // se indica que este obj no se destruya con la carga de escenas
         }
-        else //si instance tiene info
+        else // si instance tiene info
         {
-            Destroy(gameObject); //se destruye el gameObject, para que no haya dos o mas gms en el juego
+            Destroy(gameObject); // se destruye el gameObject, para que no haya dos o mas gms en el juego
         }
-
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -34,8 +37,37 @@ public class GameManager : MonoBehaviour
         time += Time.deltaTime;
     }
 
+    // getter
     public float GetTime()
     {
         return time;
     }
+
+    // getter
+    public int GetPoints()
+    {
+        return points;
+    }
+
+    // setter
+    public void SetPoints(int value)
+    {
+        points = value;
+    }
+
+    // callback ---> funcion que se va a llamar en el onclick() de los botones
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+        //oye, audiomanager, Limpia todos los sonidos que estan sonando
+        AudioManager.instance.ClearAudios();
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit!!");
+        Application.Quit();
+    }
 }
+
+
