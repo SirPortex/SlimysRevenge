f3086d624580c715b27fd5199344f09d1c59467f
Pavel.K144 <portex144@gmail.com>
2024-02-09 14:05:10 +0100

    Menu and Sounds

---
+++ Assets/Scripts/ButtonFunctions.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonFunctions : MonoBehaviour
+{
+   public void ExitGame()
+    {
+        GameManager.instance.ExitGame();
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        GameManager.instance.LoadScene(sceneName);
+
+    }
+}
